939229a34c72b5f2c7462b1e02b2650907c1ed03
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-07-17 16:37:09 +0200

    Add base stuff for the registration

---
+++ api/routes/auth/register.py
@@ -0,0 +1,30 @@
+import bcrypt
+import logging
+from fastapi import APIRouter, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+from .validation_models import RegisterModel
+
+# Define global variables
+router = APIRouter()
+logger = logging.getLogger(__name__)
+
+
+class Register:
+    def __init__(self, db_session: AsyncSession, data: RegisterModel) -> None:
+        self.db_session: AsyncSession = db_session
+        self.data: RegisterModel = data
+
+    def hash_pwd(self):
+        """ Hashes the password """
+
+    async def is_email_registered(self):
+        """ Check if the email address is already registered or not """
+
+    async def create_user(self):
+        """ Create a new user """
+
+
+@router.post("/api/register")
+async def register(data: RegisterModel):
+    """ Route to register a new user """
+    return {"status_code": 201, "detail": "Account successfully created"}
