f9da25ef0117ff8386fc258afffc81943571ea3d
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-07-17 15:14:28 +0200

    Add register route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Register from "./pages/register/Register";
+
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
