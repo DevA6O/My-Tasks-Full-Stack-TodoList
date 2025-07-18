6304308753f4025c4e49b1eb2a7d6911fc70fef4
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-07-18 15:12:46 +0200

    Add error field location to the JSONResponse and add a note

--- api/exception_handler.py
+++ api/exception_handler.py
@@ -3,15 +3,19 @@ from fastapi import Request, status
 from fastapi.responses import JSONResponse
 from fastapi.exceptions import RequestValidationError
 
-logger = logging.getLogger(__name__)
+# NOTE: Validation message could be more user-friendly - maybe change later
 
 async def validation_exception_handler(request: Request, exc: RequestValidationError):
     """ Handler for validation errors (as from pydantic) """
     errors = exc.errors()
+
     return JSONResponse(
         status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
         content={
-            "detail": "Validation failed",
+            "detail": {
+                "message": errors[0].get("msg", "Unknown error occurred."),
+                "field": errors[0].get("loc", (None, None))[1]
+            },
             "errors": errors
         }
     )
