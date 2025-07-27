f6ecf2291491f5e7d0d915676adba378150fd999
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-07-27 19:10:23 +0200

    Add Todo table

--- api/database/models.py
+++ api/database/models.py
@@ -13,3 +13,13 @@ class User(Base):
     email: Mapped[str] = mapped_column(unique=True, nullable=False)
     password: Mapped[str]
     created_at: Mapped[int] = mapped_column(Integer, default=round(datetime.now().timestamp()))
+
+
+class Todo(Base):
+    __tablename__ = "todos"
+
+    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, unique=True, default=uuid.uuid4)
+    title: Mapped[str] = mapped_column(nullable=False)
+    description: Mapped[str] = mapped_column(nullable=False)
+    created_at: Mapped[int] = mapped_column(Integer, default=round(datetime.now().timestamp()))
+    edited_at: Mapped[int] = mapped_column(Integer, default=round(datetime.now().timestamp()), onupdate=round(datetime.now().timestamp()))
