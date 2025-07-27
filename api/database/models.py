feccb5d63dbf5b0cfa48e1b21d49ef4b7922961f
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-07-27 19:17:52 +0200

    Add function current_timestamp to get the current timestamp

--- api/database/models.py
+++ api/database/models.py
@@ -5,6 +5,10 @@ from sqlalchemy.orm import Mapped, mapped_column
 from database.connection import Base
 
 
+def current_timestamp() -> int:
+    return round(datetime.now().timestamp())
+
+
 class User(Base):
     __tablename__ = "users"
 
@@ -12,7 +16,7 @@ class User(Base):
     name: Mapped[str] = mapped_column(nullable=False)
     email: Mapped[str] = mapped_column(unique=True, nullable=False)
     password: Mapped[str]
-    created_at: Mapped[int] = mapped_column(Integer, default=round(datetime.now().timestamp()))
+    created_at: Mapped[int] = mapped_column(Integer, default=current_timestamp)
 
 
 class Todo(Base):
@@ -21,5 +25,5 @@ class Todo(Base):
     id: Mapped[uuid.UUID] = mapped_column(primary_key=True, unique=True, default=uuid.uuid4)
     title: Mapped[str] = mapped_column(nullable=False)
     description: Mapped[str] = mapped_column(nullable=False)
-    created_at: Mapped[int] = mapped_column(Integer, default=round(datetime.now().timestamp()))
-    edited_at: Mapped[int] = mapped_column(Integer, default=round(datetime.now().timestamp()), onupdate=round(datetime.now().timestamp()))
+    created_at: Mapped[int] = mapped_column(Integer, default=current_timestamp)
+    edited_at: Mapped[int] = mapped_column(Integer, default=current_timestamp, onupdate=current_timestamp)
