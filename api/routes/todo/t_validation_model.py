9eeba56560c503484221963d9b5838d80c1f685e
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-07-31 00:01:58 +0200

    user_id from TodoCreation validation class removed

--- api/routes/todo/t_validation_model.py
+++ api/routes/todo/t_validation_model.py
@@ -1,7 +1,5 @@
-from uuid import UUID
 from pydantic import BaseModel, Field
 
 class TodoCreation(BaseModel):
-    user_id: UUID
     title: str = Field(min_length=2, max_length=140)
     description: str = Field(min_length=0, max_length=320)
