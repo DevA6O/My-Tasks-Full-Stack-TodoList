1736d4ab424017a18ca58f57760209545a0e9a9b
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-07-31 11:17:36 +0200

    Bug fix: Whitespaces will now removed before insert

--- api/routes/todo/t_creation.py
+++ api/routes/todo/t_creation.py
@@ -18,13 +18,15 @@ class TodoCreation:
     def __init__(self, db_session: AsyncSession, data: TodoCreationValidation, user_id: UUID) -> None:
         self.db_session: AsyncSession = db_session
         self.data: TodoCreationValidation = data
+        self.title: str = self.data.title.strip()
+        self.description: str = self.data.description.strip()
         self.user_id: UUID = user_id
 
     async def insert_new_todo(self):
         """ Write the todo to the database """
         stmt = (
             insert(Todo)
-            .values(user_id=self.user_id, title=self.data.title, description=self.data.description)
+            .values(user_id=self.user_id, title=self.title, description=self.description)
             .returning(Todo)
         )
         result_obj = await self.db_session.execute(stmt)
@@ -37,20 +39,20 @@ class TodoCreation:
         
     async def is_todo_exist(self) -> bool:
         """ Check whether the task already exists or not. """
-        stmt = select(Todo).where(Todo.user_id == self.user_id, Todo.title == self.data.title)
+        stmt = select(Todo).where(Todo.user_id == self.user_id, Todo.title == self.title)
         result = await self.db_session.execute(stmt)
         return result.scalar_one_or_none()
 
     async def create(self):
         """ Creates a new todo """
         if await self.is_todo_exist():
-            return None, f"Todo ({self.data.title}) already exist."
+            return None, f"Todo ({self.title}) already exist."
 
         try:
             todo = await self.insert_new_todo()
 
             if todo:
-                return todo, f"Todo ({self.data.title}) successfully added."
+                return todo, f"Todo ({self.title}) successfully added."
         except IntegrityError as e:
             logger.exception(str(e), exc_info=True, extra={"user_id": self.user_id})
             return None, "Server error: Please try it later again."
