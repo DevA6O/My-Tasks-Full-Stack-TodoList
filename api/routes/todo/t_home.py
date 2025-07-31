4cd7aa1ea10f0e20a0bfbefcfd22df18966a3070
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-07-31 10:24:06 +0200

    Add method get_user_with_todos to make a db request for the username and the todos, add a new BaseModel TodoSchema for the endpoint /api/todo/get_all.

--- api/routes/todo/t_home.py
+++ api/routes/todo/t_home.py
@@ -1,34 +1,66 @@
+from uuid import UUID
 from fastapi import APIRouter, Depends, HTTPException, status, Header
 from fastapi.responses import JSONResponse
 from sqlalchemy import select
+from sqlalchemy.orm import selectinload
 from sqlalchemy.ext.asyncio import AsyncSession
+from typing import Tuple
+from pydantic import BaseModel, ConfigDict
 
 from database.connection import get_db
-
-fake_todos = [
-    {"title": "Trash", "description": "Take out the trash", "id": 1},
-    {"title": "Learning", "description": "Learn for the spanish test", "id": 2}
-]
+from database.models import User
+from security.jwt import decode_token
 
 router = APIRouter()
 
 class TodoHome():
-    def __init__(self, db_session: AsyncSession) -> None:
+    def __init__(self, db_session: AsyncSession, user_id: UUID) -> None:
         self.db_session: AsyncSession = db_session
+        self.user_id: UUID = user_id
 
-    async def _all_todos(self):
-        """ Database query to get every todo from user """
+    async def get_user_with_todos(self) -> Tuple[str, list]:
+        """ Returns the username and every todo the user have """
+        stmt = (
+            select(User)
+            .options(selectinload(User.todos))
+            .where(User.id == self.user_id)
+        )
+        result_obj = await self.db_session.execute(stmt)
+        user: User = result_obj.scalar_one_or_none()
 
-    async def get_all(self):
-        return fake_todos
+        if not user:
+            return "Unknown User", []
+        
+        username: str = user.name
+        todos: list = user.todos
+        return username, todos
+
+class TodoSchema(BaseModel):
+    id: UUID
+    title: str
+    description: str
+
+    model_config = ConfigDict(from_attributes=True)
 
 
 
 
 @router.post("/api/todo/get_all")
-async def get_all_todos(authorization: str = Header(None), db_session: AsyncSession = Depends(get_db)):
+async def get_all_todos(
+    authorization: str = Header(None), db_session: AsyncSession = Depends(get_db)
+) -> None:
     """ Endpoint to get all todos """
-    print(authorization[len("Bearer "):])
+    access_token: str = authorization[len("Bearer "):]
+    user_id: UUID = decode_token(token=access_token)
+
+    if user_id is None:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="The user could not be identified.")
+    
+    todo_service = TodoHome(db_session=db_session, user_id=user_id)
+    username, todos = await todo_service.get_user_with_todos()
     return JSONResponse(
-        status_code=status.HTTP_200_OK, content={"todos": fake_todos, "username": "DevA."}
+        status_code=status.HTTP_200_OK, content={
+            "username": username, 
+            "todos": [todo.model_dump(mode="json") for todo in map(TodoSchema.model_validate, todos)]
+        }
     )
