d5c30cafce2b5f84c9af3dcc63ab5b86322eee5f
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-08-01 19:01:50 +0200

    Add comments and change the helper methods to private helper methods

--- api/routes/todo/t_creation.py
+++ api/routes/todo/t_creation.py
@@ -23,37 +23,45 @@ class TodoCreation:
         self.description: str = self.data.description.strip()
         self.user_id: UUID = user_id
 
-    async def insert_new_todo(self) -> Todo | None:
-        """ Write the todo to the database """
+    async def _insert_new_todo(self) -> Todo | None:
+        """ Write the todo into the database. """
         stmt = (
             insert(Todo)
             .values(user_id=self.user_id, title=self.title, description=self.description)
             .returning(Todo)
         )
+
+        # Insert the data
         result_obj = await self.db_session.execute(stmt)
         todo_instance = result_obj.scalar_one_or_none()
         await self.db_session.commit()
 
+        # Check whether the insert was successful
         if todo_instance:
             await self.db_session.refresh(todo_instance)
-        return todo_instance
         
-    async def is_todo_exist(self) -> bool:
+        return todo_instance
+
+    async def _is_todo_exist(self) -> bool:
         """ Check whether the task already exists or not. """
         stmt = select(Todo).where(Todo.user_id == self.user_id, Todo.title == self.title)
         result = await self.db_session.execute(stmt)
         return result.scalar_one_or_none()
 
     async def create(self) -> Tuple[Tuple | None, str]:
-        """ Creates a new todo """
-        if await self.is_todo_exist():
+        """ Creates a new todo. """
+        # Check whether the todo (title) is already exist
+        if await self._is_todo_exist():
             return None, f"Todo ({self.title}) already exist."
 
         try:
-            todo = await self.insert_new_todo()
+            # Insert the todo if the todo is not exist
+            todo = await self._insert_new_todo()
 
-            if todo:
+            if todo: # If the todo successfully got inserted
                 return todo, f"Todo ({self.title}) successfully added."
+            
+        # Fallback if insertion failed due to foreign keys or other reasons
         except IntegrityError as e:
             logger.exception(str(e), exc_info=True, extra={"user_id": self.user_id})
             return None, "Server error: Please try it later again."
