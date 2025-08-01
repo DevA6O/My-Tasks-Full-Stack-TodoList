baf56404ebbf66475f0c050a7996bd6278df8a93
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-08-01 18:54:21 +0200

    File deleted.

--- api/database/events.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import event
-from sqlalchemy.engine import Engine
-
-# Only needed for sqlite
-@event.listens_for(Engine, "connect")
-def set_sqlite_pragma(dbapi_connection, connection_record):
-    cursor = dbapi_connection.cursor()
-    cursor.execute("PRAGMA foreign_keys=ON")
-    cursor.close()
