b646f24755fcf416acdbe206837e2d6e39d8e583
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-08-02 19:05:30 +0200

    Test added for get_all_todos_endpoint

--- api/tests/todo/test_t_home.py
+++ api/tests/todo/test_t_home.py
@@ -1,15 +1,24 @@
 import pytest
 import uuid
+import os
+import json
 import pytest_asyncio
+from dotenv import load_dotenv
 from unittest.mock import AsyncMock
+from httpx import AsyncClient, ASGITransport
 from sqlalchemy import delete, insert
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import Tuple
 
 from database.models import User, Todo
+from database.connection import get_db
 from routes.todo.t_home import TodoHome
 from conftest import fake_username
+from security.jwt import get_bearer_token, create_token
+from main import api
+
+load_dotenv()
 
 @pytest_asyncio.fixture
 async def fake_todo(fake_user: Tuple[User, AsyncSession]) -> Tuple[Todo, User, AsyncSession]:
@@ -82,7 +91,7 @@ def test_todo_home_class(
 async def test_get_user_with_todos(
     valid_user: bool, valid_db_session, has_todos: bool,
     expected_username: str, expected_todos: list[Todo | None], expected_error_msg: str,
-    fake_todo: Tuple[Todo, User, AsyncSession]
+    fake_todo: Tuple[Todo | None, User, AsyncSession]
 ) -> None:
     # Define the user and the db session
     todo, user, db_session = fake_todo
@@ -116,3 +125,47 @@ async def test_get_user_with_todos(
         assert error_msg is expected_error_msg
     else:
         assert isinstance(error_msg, expected_error_msg)
+
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "valid_user_token, valid_user, valid_class_params, expected_status_code",
+    [
+        (True, True, True, 200), # Success
+        (False, True, True, 400), # Invalid user token
+        (True, False, True, 400), # Invalid user
+        (True, True, False, 400), # Invalid class param(s)
+    ]
+)
+async def test_get_all_todos_endpoint(
+    valid_user_token: bool, valid_user: bool, valid_class_params: bool, 
+    expected_status_code: int, fake_todo: Tuple[Todo, User, AsyncSession]
+) -> None:
+    # Define the fake todo, fake user and the fake db session
+    todo, user, db_session = fake_todo
+    user_id = user.id if valid_user else uuid.uuid4()
+
+    # Define the usertoken
+    if valid_user_token:
+        user_token = create_token(data={"sub": str(user_id)}) # Valid
+    else:
+        user_token = create_token(data={"no_sub": str(user_id)}) # Invalid
+
+    # Define the class parmas
+    if valid_class_params:
+        api.dependency_overrides[get_db] = lambda: db_session # Valid
+    else:
+        api.dependency_overrides[get_db] = lambda: AsyncSession # Invalid
+
+    # Override the token with the created user token
+    api.dependency_overrides[get_bearer_token] = lambda: user_token
+    transport = ASGITransport(app=api)
+
+    # Start the request
+    async with AsyncClient(transport=transport, base_url=os.getenv("VITE_API_URL")) as ac:
+        response = await ac.post("/todo/get_all", json={})
+        status_code = response.status_code
+
+        # Check the response
+        assert status_code == expected_status_code
