10c37bdc9fc538ae49c4eac3f4b0c06a29b56ca1
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-08-03 22:05:45 +0200

    Named changed from TodoDeletion to TodoDeletionModel and add two optional fields but one is requried (checking with model_validator)

--- api/routes/todo/t_validation_model.py
+++ api/routes/todo/t_validation_model.py
@@ -1,14 +1,23 @@
 from uuid import UUID
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, model_validator
+from typing import Optional
 
 class TodoCreation(BaseModel):
     title: str = Field(min_length=2, max_length=140)
     description: str = Field(min_length=0, max_length=320)
 
-class TodoDeletion(BaseModel):
+class TodoDeletionModel(BaseModel):
     todo_id: UUID
     user_id: UUID
 
 class TodoExistCheckModel(BaseModel):
     user_id: UUID
-    title: str
+    title: Optional[str] = None
+    todo_id: Optional[UUID] = None
+
+    @model_validator(mode="before")
+    @classmethod
+    def validate_title_or_todoID(cls, data: dict) -> dict:
+        if not data.get("title") and not data.get("todo_id"):
+            raise ValueError("Either title or todo_id must be provided.")
+        return data
