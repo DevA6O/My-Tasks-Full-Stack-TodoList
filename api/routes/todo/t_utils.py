f198ec78b3b3dd756537473b916415205a1f126e
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-08-03 21:50:05 +0200

    Add _is_todo_exist function from t_creation.py to t_utils.py

---
+++ api/routes/todo/t_utils.py
@@ -0,0 +1,25 @@
+from uuid import UUID
+from pydantic import BaseModel
+from sqlalchemy import select, exists
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+from routes.todo.t_validation_model import TodoExistCheckModel
+from database.models import Todo
+
+async def _is_todo_exist(data: TodoExistCheckModel, db_session: AsyncSession) -> bool:
+    """ Helper-Function to check whether the task already exists or not. 
+    
+    Returns:
+    ---------
+        - A boolean
+    """
+    if not isinstance(db_session, AsyncSession):
+        raise ValueError("db_session must be an AsyncSession.")
+
+    stmt = select(
+        exists().where(Todo.user_id == data.user_id, Todo.title == data.title)
+    )
+    result = await db_session.execute(stmt)
+    return result.scalar()
