f94cfb9aac3e9a2aff22926d0059569a28b6223b
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-08-04 04:01:10 +0200

    Add new Router (TodoDeletionRouter)

--- api/routes/todo/__init__.py
+++ api/routes/todo/__init__.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 from .t_home import router as HomeRouter
 from .t_creation import router as TodoCreationRouter
+from .t_deletion import router as TodoDeletionRouter
 
 TodoRouter = APIRouter()
 TodoRouter.include_router(HomeRouter)
 TodoRouter.include_router(TodoCreationRouter)
+TodoRouter.include_router(TodoDeletionRouter)
