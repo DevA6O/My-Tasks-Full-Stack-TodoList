3b8ed792c2239a908e6d1aaf6b27decf0e6a08d1
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-08-09 13:53:23 +0200

    Bug fix: Forget to add export 'default' in function completeTodoAPI and forget to add const to the response. Both have been fixed.

--- src/pages/todo/t_completor.js
+++ src/pages/todo/t_completor.js
@@ -1,5 +1,5 @@
-export async function completeTodoAPI(todoID, accessToken) {
-    response = await fetch(`${import.meta.env.VITE_API_URL}/todo/complete`, {
+export default async function completeTodoAPI(todoID, accessToken) {
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/todo/complete`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
