b3844f28cc26da89c608b584944a52a8ddd44dbb
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-08-09 05:25:48 +0200

    Completor router added.

--- api/routes/todo/__init__.py
+++ api/routes/todo/__init__.py
@@ -3,9 +3,11 @@ from .t_home import router as HomeRouter
 from .t_creation import router as TodoCreationRouter
 from .t_deletion import router as TodoDeletionRouter
 from .t_editor import router as TodoEditorRouter
+from .t_completor import router as TodoCompletorRouter
 
 TodoRouter = APIRouter()
 TodoRouter.include_router(HomeRouter)
 TodoRouter.include_router(TodoCreationRouter)
 TodoRouter.include_router(TodoDeletionRouter)
 TodoRouter.include_router(TodoEditorRouter)
+TodoRouter.include_router(TodoCompletorRouter)
