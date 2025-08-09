86960ba2cb8bf78d8e9ba476d71bbe0987be4b09
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-08-09 03:05:19 +0200

    Feature added: validate_constructor can now also check whether db_session or user_id have the correct type. Both parameters do not have to be specified. In addition, a bug with the transfer of arguments was fixed.

--- api/routes/todo/t_utils.py
+++ api/routes/todo/t_utils.py
@@ -20,14 +20,22 @@ logger = logging.getLogger(__name__)
 
 
 def validate_constructor(method):
-    """ Validator to validate db_session and user_id """
-    def wrapper(self, db_session: AsyncSession, user_id: UUID, *args, **kwargs):
-        if not isinstance(db_session, AsyncSession):
+    """ Validator to validate the db_session and / or the user_id """
+    from functools import wraps
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        # Fetches the db_session and user_id
+        db_session = kwargs.get("db_session", None)
+        user_id = kwargs.get("user_id", None)
+
+        if db_session is not None and not isinstance(db_session, AsyncSession):
             raise ValueError("db_session must be an instance of AsyncSession.")
-        if not isinstance(user_id, UUID):
+        if user_id is not None and not isinstance(user_id, UUID):
             raise ValueError("user_id must be an instance of UUID.")
         
-        return method(self, db_session, user_id, *args, **kwargs)
+        return method(self, *args, **kwargs)
+    
     return wrapper
 
 
