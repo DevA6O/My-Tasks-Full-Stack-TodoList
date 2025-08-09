950e198732ae5e58fad8517f0c01ab7d81342e06
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-08-09 05:12:47 +0200

    Old code removed and replaced with utils functions

--- api/routes/todo/t_completor.py
+++ api/routes/todo/t_completor.py
@@ -10,8 +10,12 @@ from typing import Tuple
 from database.models import Todo
 from database.connection import get_db
 from security.jwt import get_bearer_token, decode_token
-from routes.todo.t_utils import todo_exists, TodoExistCheckModel
 from routes.todo.t_validation_model import TodoCompletorModel
+from routes.todo.t_utils import (
+    todo_exists, TodoExistCheckModel,
+    run_todo_db_statement, RunTodoDbStatementContext,
+    handle_todo_request, HandleTodoRequestModel
+)
 
 logger = logging.getLogger(__name__)
 router = APIRouter()
@@ -40,37 +44,24 @@ class TodoCompletor:
             - A boolean: To check whether the completion was successful or not
             - A string containing the completion information
         """
-        try:
-            # Checks whether the todo does not exist
-            check_data = TodoExistCheckModel(user_id=self.user_id, todo_id=self.data.todo_id)
-
-            if not await todo_exists(data=check_data, db_session=self.db_session):
-                return False, "Completion failed: Todo could not be found."
-
-            # Mark as complete
-            stmt = (
-                update(Todo)
-                .where(Todo.user_id == self.user_id, Todo.id == self.data.todo_id)
-                .values(completed=True)
-                .returning(Todo)
+        return await run_todo_db_statement(
+            ctx=RunTodoDbStatementContext(
+                data=TodoExistCheckModel(
+                    user_id=self.user_id,
+                    todo_id=self.data.todo_id
+                ),
+                db_statement=(
+                    update(Todo)
+                    .where(Todo.user_id == self.user_id, Todo.id == self.data.todo_id)
+                    .values(completed=True)
+                    .returning(Todo)
+                ),
+                db_session=self.db_session,
+                success_msg="Completion successful: Todo marked as completed successfully.",
+                default_error_msg=DEFAULT_COMPLETION_ERROR_MSG,
+                execution_type="Completion"
             )
-            result = await self.db_session.execute(stmt)
-
-            # Check whether the completion wasn't successfully
-            todo_obj = result.scalar_one_or_none()
-
-            if todo_obj is None:
-                logger.warning("Completion failed: Unknown error occurred.", extra={
-                    "user_id": self.user_id, "todo_id": self.data.todo_id
-                })
-                return False, DEFAULT_COMPLETION_ERROR_MSG
-            
-            # If the completion was successfully
-            return True, "Completion successful: Todo marked as completed successfully."
-        
-        except SQLAlchemyError as e:
-            logger.exception(f"Database error: {str(e)}", exc_info=True)
-            return False, DEFAULT_COMPLETION_ERROR_MSG
+        )
         
 
 @router.post("/api/todo/complete")
@@ -79,23 +70,12 @@ async def completor_endpoint(
     db_session: AsyncSession = Depends(get_db)
 ) -> JSONResponse:
     """ Endpoint to mark a todo as completed """
-    try:
-        # Default http exception
-        http_exception = HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=DEFAULT_COMPLETION_ERROR_MSG)
-
-        # Fetch user_id from token
-        user_id: UUID = decode_token(token=token)
-
-        # Start calling method
-        service_completor = TodoCompletor(data=data, db_session=db_session, user_id=user_id)
-        success, msg = await service_completor.mark_as_completed()
-
-        # Check whether the todo could not be marked successfully
-        if not success:
-            http_exception.detail = msg
-            raise http_exception
-        
-        raise JSONResponse(status_code=status.HTTP_200_OK, content={"message": msg})
-    except ValueError as e:
-        logger.exception(str(e), exc_info=True)
-        raise http_exception
+    return handle_todo_request(
+        data_model=data, db_session=db_session,
+        params=HandleTodoRequestModel(
+            token=token,
+            service_class=TodoCompletor,
+            service_method="mark_as_completed",
+            default_error_message=DEFAULT_COMPLETION_ERROR_MSG
+        )
+    )
