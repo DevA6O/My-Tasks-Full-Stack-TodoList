c09684194acf7f71f75092446cdd045f531d62d0
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-08-09 13:45:39 +0200

    New function added: completeTask to complete the task / todo

---
+++ src/pages/todo/t_completor.js
@@ -0,0 +1,15 @@
+export async function completeTask(todoID, accessToken) {
+    response = await fetch(`${import.meta.env.VITE_API_URL}/todo/complete`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${accessToken}`
+        },
+        body: JSON.stringify({todo_id: todoID})
+    });
+
+    if (!response.ok) {
+        data = await response.json();
+        throw new Error(data.detail || "Completion failed: An unexpected error is occurred.");
+    };
+}
