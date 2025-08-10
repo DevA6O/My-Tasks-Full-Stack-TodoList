ac1d5d87b29204efe0a13359d8694057d6c1ff4f
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-08-10 14:15:42 +0200

    Test function 'test_hash_pwd' replaced by the test class 'TestHashPwd' which is eaiser to read and understand.

--- api/tests/security/test_hashing.py
+++ api/tests/security/test_hashing.py
@@ -1,32 +1,48 @@
 import pytest
 from security.hashing import hash_pwd, is_hashed
+from conftest import fake_password
 
-@pytest.mark.parametrize(
-    "password, expected_value",
-    [
-        ("FakePassword123", str),
-        (0, ValueError),
-    ]
-)
-def test_hash_pwd(password: str, expected_value: type) -> None:
-    if expected_value is ValueError:
+
+class TestHashPwd:
+    """ Test class for different test scenarios for the hash_pwd function """
+
+    def test_hash_pwd_success(self) -> None:
+        """ Tests the success case """
+        hashed_password: str = hash_pwd(password=fake_password)
+        assert isinstance(hashed_password, str)
+
+    def test_hash_pwd_failed_because_value_error(self) -> None:
+        """ Tests the failed case when a ValueError occurrs """
         with pytest.raises(ValueError):
-            hash_pwd(password)
-        return
+            hash_pwd(password=int(0))
+
+
+# @pytest.mark.parametrize(
+#     "password, expected_value",
+#     [
+#         ("FakePassword123", str),
+#         (0, ValueError),
+#     ]
+# )
+# def test_hash_pwd(password: str, expected_value: type) -> None:
+#     if expected_value is ValueError:
+#         with pytest.raises(ValueError):
+#             hash_pwd(password)
+#         return
     
-    hashed_password: str = hash_pwd(password)
-    assert isinstance(hashed_password, expected_value)
+#     hashed_password: str = hash_pwd(password)
+#     assert isinstance(hashed_password, expected_value)
 
 
-# Hash test password for is_hashed test function
-test_hashed_pwd = hash_pwd("TestPassword123")
+# # Hash test password for is_hashed test function
+# test_hashed_pwd = hash_pwd("TestPassword123")
 
-@pytest.mark.parametrize(
-    "password, expected_value",
-    [
-        (test_hashed_pwd, True),
-        ("NotAHashedPassword", False)
-    ]
-)
-def test_is_hashed(password: str, expected_value: bool) -> None:
-    assert is_hashed(password) == expected_value
+# @pytest.mark.parametrize(
+#     "password, expected_value",
+#     [
+#         (test_hashed_pwd, True),
+#         ("NotAHashedPassword", False)
+#     ]
+# )
+# def test_is_hashed(password: str, expected_value: bool) -> None:
+#     assert is_hashed(password) == expected_value
