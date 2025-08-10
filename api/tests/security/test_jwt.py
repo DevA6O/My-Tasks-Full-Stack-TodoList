b657c9dfe3432005e152ed5e5bcf71d4f1aad337
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-08-10 14:48:35 +0200

    Test function 'test_create_token' has been replaced by the test class 'TestCreateToken', which is eaiser to read and understand.

--- api/tests/security/test_jwt.py
+++ api/tests/security/test_jwt.py
@@ -71,6 +71,39 @@ class TestDecodeToken:
         assert result is None
 
 
+class TestCreateToken:
+    """ Test class for different test scenarios for the create_token function """
+
+    @pytest.mark.parametrize("minutes", [(ACCESS_TOKEN_EXPIRE_MINUTES), (5)])
+    def test_create_token_success(self, minutes: int) -> None:
+        """ Tests the success case when an expiration date is set and when it is not """
+        if minutes == ACCESS_TOKEN_EXPIRE_MINUTES:
+            token: str = create_token(data={"sub": str(uuid.uuid4())})
+        else:
+            token: str = create_token(data={"sub": str(uuid.uuid4())}, expire_delta=timedelta(minutes=minutes))
+        assert isinstance(token, str)
+
+        # Tests the expiration
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        exp_timestamp = payload["exp"]
+        exp_datetime = datetime.fromtimestamp(exp_timestamp, tz=timezone.utc)
+
+        expected_exp = datetime.now(timezone.utc) + timedelta(minutes=minutes)
+
+        # Allows a deviation of 2 seconds
+        assert abs((exp_datetime - expected_exp).total_seconds()) < 2
+
+    @pytest.mark.parametrize("data", [(None, str("Not a dict"))])
+    def test_create_token_failed_because_data_is_set_incorrect(self, data: str | None) -> None:
+        """ Tests the failed case when the data is set incorrectly """
+        with pytest.raises(ValueError):
+            create_token(data=data)
+
+    def test_create_token_failed_because_expire_delta_is_set_incorrect(self) -> None:
+        """ Tests the failed case when expire_delta is set incorrectly """
+        with pytest.raises(ValueError):
+            create_token(data={"sub": str(uuid.uuid4())}, expire_delta=False)
+
 
 # @pytest.mark.parametrize(
 #     "data, expire_delta, expected_exception",
