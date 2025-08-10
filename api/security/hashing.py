0b1dd1795bb96b9f8a7bbac5c2be917965cf79f7
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-08-10 14:11:33 +0200

    Function description has been updated (easier to understand)

--- api/security/hashing.py
+++ api/security/hashing.py
@@ -2,14 +2,24 @@ import re
 import bcrypt
 
 def hash_pwd(password: str) -> str:
-    """ Hashes the password """
+    """ Hashes the password 
+
+    Returns:
+    ---------
+        - A decoded hashed password as string
+    """
     if not isinstance(password, str):
         raise ValueError("Password must be a string.")
 
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
 
 def is_hashed(password: str) -> bool:
-    """ Checks if the password is in a valid hashed format. """
+    """ Checks if the password is in a valid hashed format 
+    
+    Returns:
+    --------
+        - A boolean
+    """
     if isinstance(password, str):
         return re.match(r'^\$2[aby]\$\d{2}\$[./A-Za-z0-9]{53}$', password) is not None
     
