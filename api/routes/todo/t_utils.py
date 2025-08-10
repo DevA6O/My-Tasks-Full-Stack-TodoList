75195b98540df49ead7971c3ebd538edfb746b5b
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-08-10 06:06:42 +0200

    Function 'validate_constructor' moved from routes\todo\t_utils.py to shared/decorators.py

--- api/routes/todo/t_utils.py
+++ api/routes/todo/t_utils.py
@@ -19,26 +19,6 @@ if TYPE_CHECKING:
 logger = logging.getLogger(__name__)
 
 
-def validate_constructor(method):
-    """ Validator to validate the db_session and / or the user_id """
-    from functools import wraps
-
-    @wraps(method)
-    def wrapper(self, *args, **kwargs):
-        # Fetches the db_session and user_id
-        db_session = kwargs.get("db_session", None)
-        user_id = kwargs.get("user_id", None)
-
-        if db_session is not None and not isinstance(db_session, AsyncSession):
-            raise ValueError("db_session must be an instance of AsyncSession.")
-        if user_id is not None and not isinstance(user_id, UUID):
-            raise ValueError("user_id must be an instance of UUID.")
-        
-        return method(self, *args, **kwargs)
-    
-    return wrapper
-
-
 async def todo_exists(data: TodoExistCheckModel, db_session: AsyncSession) -> bool:
     """ Helper-Function to check whether the task already exists or not. 
     
