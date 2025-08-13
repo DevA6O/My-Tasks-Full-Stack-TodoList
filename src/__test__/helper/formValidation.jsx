42a3c4323a6181a02a6e034645b31c19fae970d8
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-08-13 22:30:46 +0200

    checkFormValidation helper function has been added

---
+++ src/__test__/helper/formValidation.jsx
@@ -0,0 +1,16 @@
+import userEvent from "@testing-library/user-event";
+import { screen } from "@testing-library/react";
+import { expect } from "vitest";
+
+export default async function checkFormValidation(
+    inputElement, inputValue, submitButton, errorMsg
+) {
+    if (inputValue !== "") {
+        await userEvent.type(inputElement, inputValue);
+    };
+    await userEvent.click(submitButton);
+
+    // Fetch displayed error message and check whether it displayed correctly
+    const displayedErrorMsg = await screen.findByText(errorMsg);
+    expect(displayedErrorMsg).not.toBeNull();
+};
