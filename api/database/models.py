88bcea78ee3a6f6a9d3a45b9aaa547fa8d926e54
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-08-16 02:37:27 +0200

    Add Auth model for tokens and security

--- api/database/models.py
+++ api/database/models.py
@@ -18,12 +18,40 @@ class User(Base):
     password: Mapped[str]
     created_at: Mapped[int] = mapped_column(Integer, default=current_timestamp)
 
+    auth: Mapped[list["Auth"]] = relationship(
+        back_populates="user",
+        cascade="all, delete-orphan",
+        passive_deletes=True
+    )
+
     todos: Mapped[list["Todo"]] = relationship(
         back_populates="user",
         cascade="all, delete-orphan",
         passive_deletes=True,
         order_by=lambda: (Todo.completed.asc(), desc(Todo.edited_at), desc(Todo.created_at))
     )
+
+class Auth(Base):
+    __tablename__ = "auth"
+
+    jti_id: Mapped[uuid.UUID] = mapped_column(primary_key=True, unique=True, default=uuid.uuid4)
+    user_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("users.id"), nullable=False)
+
+    ip_address: Mapped[str]
+    user_agent: Mapped[str]
+    device: Mapped[str]
+    browser: Mapped[str]
+    os: Mapped[str]
+
+    is_refresh_token: Mapped[bool]
+    revoked: Mapped[bool] = mapped_column(default=False)
+    expires_at: Mapped[int]
+
+    created_at: Mapped[int] = mapped_column(Integer, default=current_timestamp)
+    updated_at: Mapped[int] = mapped_column(Integer, default=current_timestamp, onupdate=current_timestamp)
+
+    user: Mapped["User"] = relationship(back_populates="auth")
+
 
 
 class Todo(Base):
