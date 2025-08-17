b25e068ce950970130256d04ae396207718f1174
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-08-17 19:47:41 +0200

    Add signout router

--- api/routes/auth/__init__.py
+++ api/routes/auth/__init__.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 from routes.auth.login import router as LoginRouter
 from routes.auth.register import router as RegisterRouter
+from routes.auth.signout import router as SignoutRouter
 
 AuthRouter = APIRouter()
 
 AuthRouter.include_router(LoginRouter)
 AuthRouter.include_router(RegisterRouter)
+AuthRouter.include_router(SignoutRouter)
