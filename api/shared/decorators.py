ae349431511085cb9fef6f66525ea2513f3ba41d
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-08-17 09:18:56 +0200

    Add a universal validator for classes and constructors - Rename validate_constructor to validate_params

--- api/shared/decorators.py
+++ api/shared/decorators.py
@@ -1,21 +1,28 @@
 from uuid import UUID
 from sqlalchemy.ext.asyncio import AsyncSession
+from inspect import signature
 
-def validate_constructor(method):
+def validate_params(method):
     """ Validator to validate the db_session and / or the user_id """
     from functools import wraps
 
     @wraps(method)
-    def wrapper(self, *args, **kwargs):
+    def wrapper(*args, **kwargs):
+        # Bind positional args to parameter names
+        sig = signature(method)
+        bound_args = sig.bind_partial(*args, **kwargs)
+        bound_args.apply_defaults()
+        arguments = bound_args.arguments
+
         # Fetches the db_session and user_id
-        db_session = kwargs.get("db_session", None)
-        user_id = kwargs.get("user_id", None)
+        db_session = arguments.get("db_session", None)
+        user_id = arguments.get("user_id", None)
 
         if db_session is not None and not isinstance(db_session, AsyncSession):
             raise ValueError("db_session must be an instance of AsyncSession.")
         if user_id is not None and not isinstance(user_id, UUID):
             raise ValueError("user_id must be an instance of UUID.")
         
-        return method(self, *args, **kwargs)
+        return method(*args, **kwargs)
     
     return wrapper
