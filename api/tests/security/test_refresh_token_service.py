db00165065048edfc384a10fef5a5f2796952cbb
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-08-17 09:35:59 +0200

    Add TestIsRefreshTokenValidFunction test class for different test scenarios for is_refresh_token_valid

--- api/tests/security/test_refresh_token_service.py
+++ api/tests/security/test_refresh_token_service.py
@@ -1,13 +1,15 @@
 import jwt
 import json
 import uuid
+import time
 import pytest
 import pytest_asyncio
 from unittest.mock import AsyncMock, Mock, patch
 from fastapi import Request, HTTPException
 from fastapi.responses import JSONResponse
-from sqlalchemy import select
+from sqlalchemy import select, update
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.exc import SQLAlchemyError
 from datetime import datetime, timedelta, timezone
 from typing import Tuple
 
@@ -145,3 +147,98 @@ class TestSetRefreshTokenMethod:
         ):
             with pytest.raises(HTTPException):
                 await self.service.set_refresh_token()
+
+
+
+class TestIsRefreshTokenValidFunction:
+    """ Test class for different test scenarios for is_refresh_token_valid function """
+
+    @pytest_asyncio.fixture(autouse=True)
+    async def setup(self, fake_user: Tuple[User, AsyncSession]) -> None:
+        """ Set up common test data """
+        self.user, self.db_session = fake_user
+
+        # Define test values
+        self.user_agent_str = "Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) " \
+        "AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1"
+
+        self.mock_request = Mock()
+        self.mock_request.__class__ = Request
+        self.mock_request.headers = {"user-agent": self.user_agent_str}
+        self.mock_request.client.host = "123.123.123.123"
+
+        self.service = RefreshTokenService(
+            request=self.mock_request, user_id=self.user.id,
+            db_session=self.db_session, status_code=200, content={}
+        )
+
+    @pytest.mark.asyncio
+    async def test_is_refresh_token_valid_success(self) -> None:
+        """ Tests the success case """
+        # Insert a token in database
+        refresh_token = await self.service._create_and_store_refresh_token()
+        assert isinstance(refresh_token, str)
+
+        # Check whether the inserted token is valid
+        result = await is_refresh_token_valid(user_id=self.user.id, db_session=self.db_session)
+        assert result
+
+    @pytest.mark.asyncio
+    async def test_is_refresh_token_valid_failed_because_no_token_in_db(self) -> None:
+        """ Tests the failed case when no token is in the database """
+        result = await is_refresh_token_valid(user_id=self.user.id, db_session=self.db_session)
+        assert not result
+
+    @pytest.mark.asyncio
+    async def test_is_refresh_token_valid_failed_because_token_is_revoked(self) -> None:
+        """ Tests the failed case when the token is revoked """
+        # Create a token
+        refresh_token = await self.service._create_and_store_refresh_token()
+        assert isinstance(refresh_token, str)
+
+        # Set token to revoked
+        stmt = update(Auth).where(Auth.user_id == self.user.id).values(
+            revoked=True
+        )
+        result = await self.db_session.execute(stmt)
+        await self.db_session.commit()
+
+        # Start the test
+        result = await is_refresh_token_valid(user_id=self.user.id, db_session=self.db_session)
+        assert not result
+
+    @pytest.mark.asyncio
+    async def test_is_refresh_token_valid_failed_because_token_is_expired(self) -> None:
+        """ Tests the failed case when the token is expired """
+        # Create a token
+        refresh_token = await self.service._create_and_store_refresh_token()
+        assert isinstance(refresh_token, str)
+
+        # Set token expiration to expired
+        stmt = update(Auth).where(Auth.user_id == self.user.id).values(
+            expires_at=int(datetime.now(timezone.utc).timestamp())
+        )
+        result = await self.db_session.execute(stmt)
+        await self.db_session.commit()
+
+        time.sleep(1) # <- Safety buffer
+
+        # Start the test
+        result = await is_refresh_token_valid(user_id=self.user.id, db_session=self.db_session)
+        assert not result
+
+    @pytest.mark.asyncio
+    async def test_is_refresh_token_valid_failed_because_db_is_broken(self) -> None:
+        """ Tests the failed case when the database is broken """
+        # Create a token (So that it doesn't fail because of that)
+        refresh_token = await self.service._create_and_store_refresh_token()
+        assert isinstance(refresh_token, str)
+
+        # Mock the database session
+        broken_session = AsyncMock(wraps=self.db_session)
+        broken_session.__class__ = AsyncSession
+        broken_session.execute.side_effect = SQLAlchemyError("Broken database session")
+
+        # Start the test
+        result = await is_refresh_token_valid(user_id=self.user.id, db_session=broken_session)
+        assert not result
