c241ab3792a5d2b947bc5b9dcec3c2bdf4b594f7
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-08-17 07:24:43 +0200

    Add TestExtractInformationsMethod class for different tests

--- api/tests/security/test_auth_token_service.py
+++ api/tests/security/test_auth_token_service.py
@@ -1,16 +1,20 @@
 import uuid
 import pytest
+import pytest_asyncio
 from fastapi import Request
 from datetime import datetime, timezone
 from unittest.mock import Mock
 from sqlalchemy.ext.asyncio import AsyncSession
+from typing import Tuple
+
+from database.models import User
 from security.auth_token_service import StoreAuthToken, AuthTokenDetails
 
 class TestGetIpAddressMethod:
     """ Test class for different test scenarios for _get_ip_address method """
 
     @pytest.fixture(autouse=True)
-    def setup(self, db_session: AsyncSession):
+    def setup(self, db_session: AsyncSession) -> None:
         """ Set up common test data """
         self.db_session = db_session
         self.data = AuthTokenDetails(
@@ -57,3 +61,67 @@ class TestGetIpAddressMethod:
         ip_address = service._get_ip_address()
 
         assert ip_address == ""
+
+
+class TestExtractInformationsMethod:
+    """ Test class for different test scenarios for _extract_informations method """
+
+    @pytest.fixture(autouse=True)
+    def setup(self, db_session: AsyncSession) -> None:
+        """ Set up common test data """
+        self.db_session = db_session
+        self.data = AuthTokenDetails(
+            user_id=uuid.uuid4(), 
+            jti_id=uuid.uuid4(),
+            is_refresh_token=True, 
+            expires_at=int(datetime.now(timezone.utc).timestamp())
+        )
+
+        self.user_agent_str: str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/92.0.4515.131"
+    
+    def test_extract_informations_success(self) -> None:
+        """ Tests the success case """
+        mock_request = Mock()
+        mock_request.__class__ = Request
+        mock_request.headers = {"user-agent": self.user_agent_str}
+
+        # Call the method to test
+        ip_address = "123.123.123.123"
+        service = StoreAuthToken(request=mock_request, data=self.data, db_session=self.db_session)
+        stmt = service._extract_informations(ip_address=ip_address)
+
+        # Compile the statement to check the values
+        compiled = stmt.compile()
+        params = compiled.params
+        import logging
+        logging.info(params)
+        # Check the values
+        assert params["ip_address"] == ip_address
+        assert params["user_agent"].startswith("Mozilla")
+        assert params["browser"] == "Chrome"
+        assert params["device"] == "Other"
+        assert params["os"] == "Windows"
+        assert params["is_refresh_token"]
+        
+    def test_extract_informations_failed_because_no_user_agent(self) -> None:
+        """ Tests the failed case when no user-agent is given """
+        mock_request = Mock()
+        mock_request.__class__ = Request
+        mock_request.headers = {}
+
+        # Call the method to test
+        ip_address = "123.123.123.123"
+        service = StoreAuthToken(request=mock_request, data=self.data, db_session=self.db_session)
+        stmt = service._extract_informations(ip_address=ip_address)
+
+        # Compile the statement to check the values
+        compiled = stmt.compile()
+        params = compiled.params
+
+        # Check the values
+        assert params["ip_address"] == "123.123.123.123"
+        assert params["user_agent"] == ""
+        assert params["device"] == "Other"
+        assert params["browser"] == "Other"
+        assert params["os"] == "Other"
+        assert params["is_refresh_token"]
