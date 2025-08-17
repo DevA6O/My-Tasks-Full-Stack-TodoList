a4d3a2f6afeeea862531b3573205651f37f7ce63
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-08-18 01:39:23 +0200

    Update signout_endpoint to make the code easier to read and understand.

--- api/routes/auth/signout.py
+++ api/routes/auth/signout.py
@@ -1,15 +1,10 @@
-import jwt
-import uuid
 import logging
-from sqlalchemy import delete
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import APIRouter, Request, Depends, HTTPException, status
 from fastapi.responses import JSONResponse
 
-from security import SECRET_KEY, ALGORITHM
-from security.jwt import get_bearer_token
-# from security.refresh_token_service import is_refresh_token_valid
+from security.refresh_token_service import RefreshTokenVerifier
 from database.connection import get_db
 from database.models import Auth
 
@@ -17,58 +12,31 @@ router = APIRouter()
 logger = logging.getLogger(__name__)
 
 
-# PROTOTYPE = NOT FOR FINAL USE!
+@router.post("/api/signout")
+async def signout_endpoint(request: Request, db_session: AsyncSession = Depends(get_db)) -> None:
+    """ Endpoint to logout an user """    
+    try:
+        # Default http exception
+        http_exception = HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
 
-# @router.post("/api/signout")
-# async def signout_endpoint(
-#     request: Request, 
-#     db_session: AsyncSession = Depends(get_db)
-# ) -> None:
-#     """ Endpoint to logout an user """
-#     refresh_token = request.cookies.get("refresh_token")
+        # Verify the refresh token
+        verifier = RefreshTokenVerifier(request=request, db_session=db_session)
+        auth_obj: Auth = await verifier.is_valid()
 
-#     logger.info(f"Headers: {dict(request.headers)}")
+        # Check whether the token is valid
+        if auth_obj: # <- Security, in case something goes wrong, but not necessarily
+            auth_obj.revoked = True
+            await db_session.commit()
 
-#     if not refresh_token:
-#         logger.info("You are not logged in.")
-#         raise HTTPException(
-#             status_code=status.HTTP_400_BAD_REQUEST,
-#             detail="You are not logged in."
-#         )
+            return JSONResponse(
+                status_code=status.HTTP_200_OK,
+                content={"message": "You have successfully logged out."}
+            )
+    except SQLAlchemyError as e:
+        logger.exception(f"Database error: {str(e)}", exc_info=True)
+
+    except ValueError as e:
+        logger.exception(f"ValueError: {str(e)}", exc_info=True)
     
-#     try:
-#         payload = jwt.decode(refresh_token, SECRET_KEY, algorithms=[ALGORITHM])
-#         user_id: str = payload.get("sub")
-#         jti_id: str = payload.get("jti")
-
-#         if not user_id or not jti_id:
-#             logger.info("You could not be identified.")
-#             raise HTTPException(
-#                 status_code=status.HTTP_400_BAD_REQUEST,
-#                 detail="You could not be identified."
-#             )
-
-#         if await is_refresh_token_valid(jti_id=uuid.UUID(jti_id), user_id=uuid.UUID(user_id), db_session=db_session):
-#             stmt = delete(Auth).where(
-#                 Auth.user_id == uuid.UUID(user_id),
-#                 Auth.jti_id == uuid.UUID(jti_id)
-#             )
-#             result = await db_session.execute(stmt)
-
-#             if result.rowcount > 0:
-#                 await db_session.commit()
-#                 return JSONResponse(
-#                     status_code=status.HTTP_200_OK,
-#                     content={"message": "You have successfully logged out."}
-#                 )
-#     except SQLAlchemyError as e:
-#         logger.exception(f"Database error: {str(e)}", exc_info=True)
-
-#     except ValueError as e:
-#         logger.exception(f"ValueError: {str(e)}", exc_info=True)
-
-#     logger.info("An unexpected error occurred: Please try again later.")
-#     raise HTTPException(
-#         status_code=status.HTTP_400_BAD_REQUEST,
-#         detail="An unexpected error occurred: Please try again later."
-#     )
+    http_exception.detail = "An unexpected error occurred: Please try again later."
+    raise http_exception
