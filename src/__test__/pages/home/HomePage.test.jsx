6fc795437f399f22e0a8402d8e5f61e1fe8032d7
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-08-19 05:50:52 +0200

    Add test 'HomePage shows elements correctly'

---
+++ src/__test__/pages/home/HomePage.test.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { beforeEach, describe, expect, it } from "vitest";
+
+import { setMockUseAuth } from "../../helper/mockUseAuth";
+import HomePage from "../../../pages/home/homepage";
+
+
+describe(HomePage, async () => {
+    beforeEach(() => {
+        setMockUseAuth({accessToken: "fake-token", loading: false});
+        render(<HomePage />);
+    });
+
+    it("HomePage shows elements correctly", async () => {
+        // Check that the loading screen has been removed correctly
+        const loadingScreenElement = screen.queryByTestId("loading-text");
+        expect(loadingScreenElement).not.toBeInTheDocument();
+
+        // Check that the navigation is displayed correctly
+        const homePageNavigation = await screen.findByTestId("HomePageNavigation");
+        expect(homePageNavigation).toBeInTheDocument();
+        
+        // Check that the main content is displayed correctly
+        const mainContent = await screen.findByTestId("homepage-main-content");
+        expect(mainContent).toBeInTheDocument();
+
+        // Check that the greeting is displayed correctly
+        const welcomeMessage = await screen.findByText(/welcome back, user!/i);
+        expect(welcomeMessage).toBeInTheDocument();
+    });
+})
