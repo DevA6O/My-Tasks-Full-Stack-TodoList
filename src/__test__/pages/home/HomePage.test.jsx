3e3ac404c91290389a59fe8609ef9b9326091bd7
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-08-19 11:32:21 +0200

    Add 'HomePage displays an error because tasks could not be loaded successful'

--- src/__test__/pages/home/HomePage.test.jsx
+++ src/__test__/pages/home/HomePage.test.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { render, screen, waitForElementToBeRemoved } from "@testing-library/react";
-import { beforeEach, describe, expect, it } from "vitest";
+import { cleanup, render, screen, waitForElementToBeRemoved } from "@testing-library/react";
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 
 import { setMockUseAuth } from "../../helper/mockUseAuth";
 import HomePage from "../../../pages/home/homepage";
@@ -9,12 +9,19 @@ import HomePage from "../../../pages/home/homepage";
 describe(HomePage, async () => {
     beforeEach(async () => {
         setMockUseAuth({accessToken: "fake-token", loading: false});
-        render(<HomePage />);
-        
-        await waitForElementToBeRemoved(() => screen.queryByTestId("loading-text"));
+        global.fetch = vi.fn();
     });
 
+    afterEach(() => {
+        cleanup();
+    });
+
+
     it("HomePage shows elements correctly", async () => {
+        // Render page and wait until the loading screen is removed
+        render(<HomePage />);
+        await waitForElementToBeRemoved(() => screen.queryByTestId("loading-text"));
+
         // Check that the loading screen has been removed correctly
         const loadingScreenElement = screen.queryByTestId("loading-text");
         expect(loadingScreenElement).not.toBeInTheDocument();
@@ -30,5 +37,57 @@ describe(HomePage, async () => {
         // Check that the greeting is displayed correctly
         const welcomeMessage = await screen.findByText(/welcome back, user!/i);
         expect(welcomeMessage).toBeInTheDocument();
+    });
+
+
+
+    it("HomePage is loading tasks successful", async () => {
+        // Mock api response from useEffect loadTasks
+        fetch.mockResolvedValueOnce({
+            ok: true,
+            status: 200,
+            json: async () => ({
+                todos: [
+                    {
+                        id: 1,
+                        title: "Todo",
+                        description: "",
+                        completed: false
+                    }
+                ],
+                username: "TestUser"
+            })
+        });
+
+        // Render page and wait until the loading screen is removed
+        render(<HomePage />);
+        await waitForElementToBeRemoved(() => screen.queryByTestId("loading-text"));
+
+        // Check that the username is displayed correctly
+        const welcomeMessage = await screen.findByText(/welcome back, testuser!/i);
+        expect(welcomeMessage).toBeInTheDocument();
+
+        // Check that the todo is displayed correctly
+        const todoElement = await screen.findByTestId("task-1");
+        expect(todoElement).toBeInTheDocument();
+    });
+
+
+
+    it("HomePage displays an error because tasks could not be loaded successful", async () => {
+        // Mock api response from useEffect loadTasks
+        fetch.mockResolvedValueOnce({
+            ok: false,
+            status: 400,
+            json: async () => ({})
+        });
+
+        // Render page and wait until the loading screen is removed
+        render(<HomePage />);
+        await waitForElementToBeRemoved(() => screen.queryByTestId("loading-text"));
+
+        // Check that the error message is displayed correctly
+        const taskErrorMessage = await screen.findByText("Server error: Tasks could not be loaded.");
+        expect(taskErrorMessage).toBeInTheDocument();
     });
 })
