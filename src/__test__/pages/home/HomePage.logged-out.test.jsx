805d12793e7d6039e7b80cc4fdf8e7f155b41d65
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-08-19 06:10:40 +0200

    Add 2 tests: Check that the loading screen is displaying correctly and check that the redirection is successful

---
+++ src/__test__/pages/home/HomePage.logged-out.test.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { describe, expect, it } from "vitest";
+import { render, screen } from "@testing-library/react";
+
+import { setMockUseAuth } from "../../helper/mockUseAuth";
+import HomePage from "../../../pages/home/homepage";
+
+
+describe("HomePage - not logged in", async () => {
+    it("HomePage shows a loading screen", async () => {
+        setMockUseAuth({accessToken: null, loading: true});
+        render(<HomePage />);
+        
+        // Check that the loading screen is displayed correctly
+        const loadingScreenElement = await screen.getByTestId("loading-text");
+        expect(loadingScreenElement).toBeInTheDocument();
+    });
+
+    it("HomePage redirect user to /login", async () => {
+        // Spy on location
+        delete window.location;
+        window.location = {href: ""};
+
+        // Render homepage and mock useAuth
+        setMockUseAuth({accessToken: null, loading: false});
+        render(<HomePage />);
+
+        // Check whether the redirection is successful
+        expect(window.location.href).toBe("/login");
+    })
+})
