faab7c34d140e6e926c502ef9e7522ab925f65a2
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-08-20 21:26:56 +0200

    Add test: HomePageAddTodo successfully adds the task

--- src/__test__/pages/home/H_AddTodo.test.jsx
+++ src/__test__/pages/home/H_AddTodo.test.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import userEvent from "@testing-library/user-event";
-import { render, screen, waitForElementToBeRemoved } from "@testing-library/react";
-import { describe, expect, it } from "vitest";
+import { cleanup, render, screen, waitForElementToBeRemoved } from "@testing-library/react";
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
+import { ToastContainer } from "react-toastify";
 
 import { setMockUseAuth } from "../../helper/mockUseAuth";
 import HomePage from "../../../pages/home/homepage";
@@ -9,6 +10,13 @@ import HomePageAddTodo from "../../../pages/home/H_AddTodo";
 
 
 describe(HomePageAddTodo, async () => {
+    beforeEach(() => {
+        global.fetch = vi.fn();
+    });
+
+    afterEach(() => {
+        cleanup();
+    })
 
     it("HomePageAddTodo loads correctly in HomePage", async () => {
         setMockUseAuth({accessToken: "fake-token", loading: false});
@@ -19,4 +27,46 @@ describe(HomePageAddTodo, async () => {
         const homePageAddTodoContent = await screen.findByTestId("HomePageAddTodo");
         expect(homePageAddTodoContent).toBeInTheDocument();
     });
-})
+
+    it("HomePageAddTodo successfully adds the task", async () => {
+        // Mock onSuccess
+        const mockOnSuccess = vi.fn();
+
+        // Mock API response
+        fetch.mockResolvedValueOnce({
+            ok: true,
+            status: 200,
+            json: async () => ({})
+        });
+
+        render(
+            <>
+                <HomePageAddTodo 
+                    accessToken={null}
+                    onSuccess={mockOnSuccess}
+                />
+
+                <ToastContainer />
+            </>   
+        );
+
+        // Check whether the input fields are present
+        const titleInput = await screen.findByTestId("HomePageAddTodo-Title-Input");
+        expect(titleInput).toBeInTheDocument();
+
+        const descriptionInput = await screen.findByTestId("HomePageAddTodo-Description-Input");
+        expect(descriptionInput).toBeInTheDocument();
+
+        const submitButton = await screen.findByTestId("HomePageAddTodo-Submit-Button");
+        expect(submitButton).toBeInTheDocument();
+
+        // Add new todo
+        await userEvent.type(titleInput, "Title");
+        await userEvent.type(descriptionInput, "Description");
+        await userEvent.click(submitButton);
+
+        // Check whether the success message is displayed correctly
+        const successMessage = await screen.findByText("Creation successful: Todo was created successfully.");
+        expect(successMessage).toBeInTheDocument();
+    });
+});
