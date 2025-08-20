5177e619e9d744fdf78a6dd21f35b526aca26a8a
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-08-20 03:40:57 +0200

    Add test which deletes or complete a task successfully

--- src/__test__/pages/home/H_ManageAndDisplayTodos.test.jsx
+++ src/__test__/pages/home/H_ManageAndDisplayTodos.test.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { beforeEach, describe, expect, it, vi } from "vitest";
+import { ToastContainer } from "react-toastify";
 
 import HomePageManageAndDisplayTodos from "../../../pages/home/H_ManageAndDisplayTodos";
 
@@ -36,5 +37,53 @@ describe(HomePageManageAndDisplayTodos, async () => {
 
         const deleteButton = await screen.findByTestId("HomePageManageAndDisplayTodos-Delete-Button-For-1");
         expect(deleteButton).toBeInTheDocument();
+    });
+
+
+    it.each([
+        [
+            "Complete", "HomePageManageAndDisplayTodos-Complete-Button-For-1", 
+            "Completion successful: Todo has been marked as successfully completed."
+        ],
+
+        [
+            "Delete", "HomePageManageAndDisplayTodos-Delete-Button-For-1", 
+            "Deletion successful: Todo has been successfully deleted."
+        ]
+    ])("HomePageManageAndDisplayTodos - %s a task successfully", async (type, testID, message) => {
+        // Mock the setReloadTasks function
+        const mockSetReloadTasks = vi.fn();
+
+        // Mock API response
+        fetch.mockResolvedValueOnce({
+            ok: true,
+            status: 200,
+            json: async () => ({})
+        });
+
+        // Render page and ToastContainer
+        render(
+            <>
+                <HomePageManageAndDisplayTodos 
+                    tasks={[{id: 1, title: "Todo", description: "Unique description", completed: false}]}
+                    accessToken={null}
+                    setReloadTasks={mockSetReloadTasks}
+                />
+
+                <ToastContainer />
+            </>
+        );
+
+        // Check and click on the button
+        const button = await screen.findByTestId(testID);
+        expect(button).toBeInTheDocument();
+
+        await userEvent.click(button);
+
+        // Check whether the action was successful
+        expect(mockSetReloadTasks).toHaveBeenCalledWith(true);
+
+        const successMessage = await screen.findByText(message);
+        expect(successMessage).toBeInTheDocument();
     });
 })
