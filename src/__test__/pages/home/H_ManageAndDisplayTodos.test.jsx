6b947f13d4dd85825093a8e5ba30610e80f5242b
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-08-20 03:51:05 +0200

    Add test that tests the error case when deletion or completion has failed.

--- src/__test__/pages/home/H_ManageAndDisplayTodos.test.jsx
+++ src/__test__/pages/home/H_ManageAndDisplayTodos.test.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { beforeEach, describe, expect, it, vi } from "vitest";
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 import { ToastContainer } from "react-toastify";
 
 import HomePageManageAndDisplayTodos from "../../../pages/home/H_ManageAndDisplayTodos";
@@ -9,6 +9,10 @@ import HomePageManageAndDisplayTodos from "../../../pages/home/H_ManageAndDispla
 describe(HomePageManageAndDisplayTodos, async () => {
     beforeEach(() => {
         global.fetch = vi.fn();
+    });
+
+    afterEach(() => {
+        vi.resetAllMocks();
     });
 
     it("HomePageManageAndDisplayTodos displays all tasks that the user has", async () => {
@@ -85,5 +89,53 @@ describe(HomePageManageAndDisplayTodos, async () => {
 
         const successMessage = await screen.findByText(message);
         expect(successMessage).toBeInTheDocument();
+    });
+
+
+    it.each([
+        [
+            "Complete", "HomePageManageAndDisplayTodos-Complete-Button-For-1",
+            "Completion failed: An unexpected error is occurred. Please try again later."
+        ],
+
+        [
+            "Delete", "HomePageManageAndDisplayTodos-Delete-Button-For-1", 
+            "Deletion failed: An unexpected error occurred. Please try again later."
+        ]
+    ])("HomePageManageAndDisplayTodos - Failed to '%s' a task", async (type, testID, message) => {
+        // Mock the setReloadTasks function
+        const mockSetReloadTasks = vi.fn();
+
+        // Mock API response
+        fetch.mockResolvedValueOnce({
+            ok: false,
+            status: 400,
+            json: async () => ({})
+        });
+
+        // Render page and ToastContainer
+        render(
+            <>
+                <HomePageManageAndDisplayTodos 
+                    tasks={[{id: 1, title: "Todo", description: "Unique description", completed: false}]}
+                    accessToken={null}
+                    setReloadTasks={mockSetReloadTasks}
+                />
+
+                <ToastContainer />
+            </>
+        );
+
+        // Check and click on the button
+        const button = await screen.findByTestId(testID);
+        expect(button).toBeInTheDocument();
+
+        await userEvent.click(button);
+
+        // Check whether failed message is displayed correctly
+        expect(mockSetReloadTasks).not.toHaveBeenCalled();
+
+        const failedMessage = await screen.findByText(message);
+        expect(failedMessage).toBeInTheDocument();
     });
 })
