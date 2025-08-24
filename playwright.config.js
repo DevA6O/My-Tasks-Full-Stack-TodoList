84647436a57134b3e3c5e28149507407fb58a693
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-08-24 04:13:55 +0200

    Add globalSetup and use

--- playwright.config.js
+++ playwright.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from '@playwright/test';
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export default defineConfig({
   testDir: "./src/__test__",
   testMatch: "**/*.spec.js",
   timeout: 30000,
+  globalSetup: "./global-setup.js",
+
+  use: {
+    baseURL: process.env.VITE_BASE_URL,
+    storageState: "./authLogin.json"
+  }
 });
