bf881226ca2ab9ed6c3861b88f355a5ba3b3bb67
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-08-25 13:03:29 +0200

    Change how engine is defined

--- api/database/connection.py
+++ api/database/connection.py
@@ -8,15 +8,7 @@ from database.config import get_db_url
 logger = logging.getLogger(__name__)
 DB_URL, TEST_MODE = get_db_url()
 
-
-# Database connection setup
-if not TEST_MODE:
-    engine = create_async_engine(DB_URL, echo=False)
-elif TEST_MODE:
-    # (WARNING): Database engine ONLY for TESTS 
-    from sqlalchemy.pool import StaticPool
-    engine = create_async_engine(DB_URL, echo=False, connect_args={"check_same_thread": False}, poolclass=StaticPool)
-
+engine = create_async_engine(DB_URL, echo=False)
 async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 Base = declarative_base()
 
@@ -34,7 +26,7 @@ async def get_db():
     async with async_session() as session:
         try:
             if TEST_MODE:
-                logger.info("Running in test mode, using in-memory database.")
+                logger.info("Running in test mode, using test database.")
 
             yield session
         finally:
