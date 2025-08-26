f64cfb593efaa63aabdf07f3ed3663a550255246
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-08-26 07:15:40 +0200

    Fix bug for 'Loading tasks was successful (without tasks)'

--- src/__test__/pages/home/home-api-tests/HomePage.spec.js
+++ src/__test__/pages/home/home-api-tests/HomePage.spec.js
@@ -1,18 +1,30 @@
 import test, { expect } from "@playwright/test";
 
-// This file does not create any to-dos to check whether they are loaded correctly. 
-// This is done separately by the responsible Add Todo test file.
+test("Loading tasks was successful (without tasks)", async ({ page, context }) => {
+    // Clear cookies
+    await context.clearCookies();
 
-test("Loading tasks was successful (without tasks)", async ({ page }) => {
-    // Go to homepage
-    await page.goto("/");
+    // Register new
+    await page.goto("/register");
+
+    await page.fill('input[name="username"]', "TestUserForTasks");
+    await page.fill('input[name="email"]', "test.frontend.tasks@email.com");
+    await page.fill('input[name="password"]', "password1234!");
+    await page.click('button[type="submit"]');
+
+    // Wait for redirection
+    await expect(page).toHaveURL("/", {timeout: 5000});
+
+    // Wait for loading
+    const loading = page.getByText("Loading...");
+    await expect(loading).not.toBeVisible();
 
     // Check whether the username is displayed correctly
-    const username = page.getByText("Welcome back, TestFrontendUser!");
+    const username = page.getByText("Welcome back, TestUserForTasks!", {timeout: 5000});
     await expect(username).toBeVisible();
 
     // Check whether the no task message is displayed and loaded correctly
-    const noTaskMessage = page.getByText("Nice work! Currently you have no tasks to solve!");
+    const noTaskMessage = page.getByText("Nice work! Currently you have no tasks to solve!", {timeout: 5000});
     await expect(noTaskMessage).toBeVisible();
 });
 
