409888b4e6d3ee72941e8ae865d2836980178747
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-08-28 11:17:09 +0200

    Add SessionHandler as router

--- api/routes/settings/__init__.py
+++ api/routes/settings/__init__.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 from routes.settings.s_service import router as ServiceRouter
+from routes.settings.s_session_handler import router as SessionHandler
 
 SettingsRouter = APIRouter()
 SettingsRouter.include_router(ServiceRouter)
+SettingsRouter.include_router(SessionHandler)
