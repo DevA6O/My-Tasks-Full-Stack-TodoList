764d2a7f5f9966750491670903e9f2ac32068640
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-08-28 19:35:29 +0200

    Update code: Make code easier to read.

--- api/routes/settings/s_service.py
+++ api/routes/settings/s_service.py
@@ -5,7 +5,7 @@ from fastapi.responses import JSONResponse
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.exc import SQLAlchemyError
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 from pydantic import BaseModel
 
 from security.jwt import get_bearer_token, decode_token
@@ -16,20 +16,69 @@ from database.models import User, Auth
 router = APIRouter()
 logger = logging.getLogger(__name__)
 
+class SessionSchema(BaseModel):
+    """ Perfect scheme for the sessions, with the most essential values """
+
+    jti_id: UUID
+    ip_address: str
+    browser: str
+    os: str
+
 class SettingsService:
     @validate_params
-    def __init__(self, user_id: UUID, db_session: AsyncSession) -> None:
-        self.user_id: UUID = user_id
+    def __init__(self, payload: dict, db_session: AsyncSession) -> None:
+        # Validate param
+        if not isinstance(payload, dict):
+            raise TypeError("payload must be a dict.")
+
+        self.payload: dict = payload
         self.db_session: AsyncSession = db_session
 
-    async def _get_sessions(self) -> List:
-        """ Returns all active sessions """
+        # Define values
+        self.user_id_str: str = self.payload.get("sub")
+        self.jti_id_str: str = self.payload.get("session_id")
+
+        # Validate params
+        if not self.user_id_str:
+            raise ValueError("Authentication failed: Unknown user.")
+        
+        if not self.jti_id_str:
+            raise ValueError("Authentication failed: Server could not verify the user.")
+        
+        # Define values with correct type
+        self.user_id: UUID = UUID(self.user_id_str)
+
+
+    async def _get_sessions(self) -> List[Dict[str, str]]:
+        """ Helper-Method: Returns a list of sessions with the schema 'SessionSchema' 
+        
+        Returns:
+        --------
+            - A List contains:
+                - A dictionary with active sessions
+        """
         stmt = select(Auth).where(Auth.user_id == self.user_id, Auth.revoked == False)
         result = await self.db_session.execute(stmt)
-        return result.scalars().all()
+        session_objs = result.scalars().all()
+
+        return [
+            {
+                **SessionSchema.model_validate(session, from_attributes=True).model_dump(mode="json"),
+                "current": str(session.jti_id) == str(self.jti_id_str)
+            } 
+            for session in session_objs
+        ]
+
     
-    async def _get_username_and_email(self) -> Tuple[str, str]:
-        """ Returns username and email """
+    async def _get_username_and_email(self) -> Tuple[str | None, str | None]:
+        """ Helper-Method: Returns the username and email address 
+        
+        Returns:
+        --------
+            - A Tuple contains:
+                - (str): Username
+                - (str): Email address
+        """
         stmt = select(User).where(User.id == self.user_id)
         result = await self.db_session.execute(stmt)
         user_obj: User = result.scalar_one_or_none()
@@ -42,8 +91,17 @@ class SettingsService:
         
         return None, None
     
-    async def get(self) -> dict:
-        """ Returns user information and sessions """
+    
+    async def get(self) -> Dict[str, str | List]:
+        """ Handler for user information
+         
+        Returns:
+        --------
+            - A Dictionary containing the following keys:
+                - (str): Username
+                - (email): Email address
+                - (sessions): A list containing one or more dictionaries with different active sessions
+        """
         try:
             sessions: List = await self._get_sessions()
             username, email = await self._get_username_and_email()
@@ -58,59 +116,34 @@ class SettingsService:
             return {}
 
 
-class SessionSchema(BaseModel):
-    jti_id: UUID
-    ip_address: str
-    browser: str
-    os: str
-
-    class Config:
-        from_attributes = True 
-
-
 @router.post("/api/settings/service")
 async def settings_service_endpoint(
     token: str = Depends(get_bearer_token), db_session: AsyncSession = Depends(get_db)
 ) -> JSONResponse:
     """ Endpoint to get the user information and sessions """
     try:
+        # Define default http exception
         http_exception = HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Fetch failed: Information about the user could not be found. Please try again later."
+            detail="Loading failed: An unknown error has occurred. Please try again later."
         )
 
+        # Decode token
         payload: dict = decode_token(token=token)
-        user_id_str: str = payload.get("sub")
-        session_id_str: str = payload.get("session_id")
-        
-        if not user_id_str:
-            raise ValueError("Token error: User ID is not included in the token.")
-        
-        if not session_id_str:
-            raise ValueError("Token error: Session ID is not included in the token.")
-        
-        service = SettingsService(user_id=UUID(user_id_str), db_session=db_session)
+
+        # Get the informations
+        service = SettingsService(payload=payload, db_session=db_session)
         informations = await service.get()
 
         if informations:
-            return JSONResponse(
-                status_code=status.HTTP_200_OK, 
-                content={
-                    "message": "Fetch successful: Information about the user could be found.",
-                    "informations": {
-                        "username": informations["username"],
-                        "email": informations["email"],
-                        "sessions": [
-                            {
-                                **SessionSchema.model_validate(session).model_dump(mode="json"),
-                                "current": str(session.jti_id) == session_id_str
-                            }
-                            for session in informations["sessions"]
-                        ]
-                    }
-                }
-            )
-    except Exception:
-        logger.exception("Fetch failed: An unexpected error is occurred.", exc_info=True)
+            return JSONResponse(status_code=status.HTTP_200_OK, content={"informations": informations})
+    except TypeError as e:
+        http_exception.detail = "Server error: A server error has occurred. Please try again later."
+        http_exception.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        logger.exception(str(e), exc_info=True)
+
+    except ValueError as e:
+        http_exception.detail = str(e)
+        logger.exception(str(e), exc_info=True)
     
     raise http_exception
