07695f4ccfa82b384896c15e737772ac492c5fb2
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-10-01 06:05:17 +0200

    Fix mistake: TypeError will now raise instead of a ValueError

--- api/shared/decorators.py
+++ api/shared/decorators.py
@@ -19,9 +19,9 @@ def validate_params(method):
         user_id = arguments.get("user_id", None)
 
         if db_session is not None and not isinstance(db_session, AsyncSession):
-            raise ValueError("db_session must be an instance of AsyncSession.")
+            raise TypeError("db_session must be an instance of AsyncSession.")
         if user_id is not None and not isinstance(user_id, UUID):
-            raise ValueError("user_id must be an instance of UUID.")
+            raise TypeError("user_id must be an instance of UUID.")
         
         return method(*args, **kwargs)
     
