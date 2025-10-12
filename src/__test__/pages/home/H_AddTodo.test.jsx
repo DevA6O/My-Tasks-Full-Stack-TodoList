dd7aee243ba9987d35bb68fc7b66d6ddf51a2d4f
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-10-12 21:14:05 +0200

    Add new test: 'Authentication failed when adding a new todo'

--- src/__test__/pages/home/H_AddTodo.test.jsx
+++ src/__test__/pages/home/H_AddTodo.test.jsx
@@ -31,7 +31,7 @@ describe(HomePageAddTodo, async () => {
 
     it.each([
         [
-            "HomePageAddTodo successfully adds the task",
+            "Todo has been successfully added",
             {
                 ok: true,
                 status: 200,
@@ -41,7 +41,7 @@ describe(HomePageAddTodo, async () => {
         ],
 
         [
-            "HomePageAddTodo was unable to add a task",
+            "Todo could not be added successfully",
             {
                 ok: false,
                 status: 400,
@@ -49,10 +49,7 @@ describe(HomePageAddTodo, async () => {
             },
             "Creation failed: An unexpected error has occurred. Please try again later."
         ]
-    ])("%s", async (funcDescription, mockResponse, displayedMessage) => {
-        // Mock onSuccess
-        const mockOnSuccess = vi.fn();
-
+    ])("%s", async (_, mockResponse, displayedMessage) => {
         // Mock API response
         fetch.mockResolvedValueOnce(mockResponse);
 
@@ -60,7 +57,7 @@ describe(HomePageAddTodo, async () => {
             <>
                 <HomePageAddTodo 
                     accessToken={null}
-                    onSuccess={mockOnSuccess}
+                    onSuccess={vi.fn()}
                 />
 
                 <ToastContainer />
@@ -85,32 +82,76 @@ describe(HomePageAddTodo, async () => {
     });
 
 
+    it("Authentication failed when adding a new todo", async () => {
+        // Reset location to check the redirection
+        delete window.location;
+        window.location = { href: "" };
+
+        // Mock API response
+        fetch.mockResolvedValueOnce({
+            ok: false,
+            status: 401,
+            json: async () => ({})
+        });
+
+        render(
+            <>
+                <HomePageAddTodo 
+                    accessToken={null}
+                    onSuccess={vi.fn()}
+                />
+
+                <ToastContainer />
+            </>   
+        );
+
+        // Get the input fields
+        const titleInput = await screen.findByTestId("HomePageAddTodo-Title-Input");
+        const descriptionInput = await screen.findByTestId("HomePageAddTodo-Description-Input");
+
+        // Fill the input fields
+        await userEvent.type(titleInput, "Title");
+        await userEvent.type(descriptionInput, "Description");
+
+        // Click on the submit button
+        const submitButton = await screen.findByTestId("HomePageAddTodo-Submit-Button");
+        await userEvent.click(submitButton);
+
+        // Check whether the redirection worked
+        expect(window.location.href).toBe("/login");
+    });
+
+
     it.each([
         ["Title", "", "", "Title is required."],
         ["Title", "x", "", "Title must have at least 2 characters."],
         ["Title", "x".repeat(141), "", "Title cannot have more than 140 characters."],
         ["Description", "xxx", "x".repeat(321), "Description cannot have more than 320 characters."]
-    ])("Add task shows a validation error message for '%s' input", async (type, titleValue, descriptionValue, errorMsg) => {
-        render(<HomePageAddTodo accessToken={null} onSuccess={null}/>);
+    ])
+    (
+        "Todo could not be created because a validation error occurred for the input '%s'.", 
+        async (_, titleValue, descriptionValue, errorMsg) => {
+            render(<HomePageAddTodo accessToken={null} onSuccess={null}/>);
 
-        // Get the input fields and the submit button
-        const titleInput = await screen.findByTestId("HomePageAddTodo-Title-Input");
-        const descriptionInput = await screen.findByTestId("HomePageAddTodo-Description-Input");
-        const submitButton = await screen.findByTestId("HomePageAddTodo-Submit-Button");
+            // Get the input fields and the submit button
+            const titleInput = await screen.findByTestId("HomePageAddTodo-Title-Input");
+            const descriptionInput = await screen.findByTestId("HomePageAddTodo-Description-Input");
+            const submitButton = await screen.findByTestId("HomePageAddTodo-Submit-Button");
 
-        // Check which field needs to be filled in
-        if (titleValue !== "") {
-            await userEvent.type(titleInput, titleValue);
-        };
-        if (descriptionValue !== "") {
-            await userEvent.type(descriptionInput, descriptionValue);
-        };
-        
-        // Click on the submit button
-        await userEvent.click(submitButton);
+            // Check which field needs to be filled in
+            if (titleValue !== "") {
+                await userEvent.type(titleInput, titleValue);
+            };
+            if (descriptionValue !== "") {
+                await userEvent.type(descriptionInput, descriptionValue);
+            };
+            
+            // Click on the submit button
+            await userEvent.click(submitButton);
 
-        // Check whether the error message is displayed correctly
-        const displayedErrorMessage = await screen.findByText(errorMsg);
-        expect(displayedErrorMessage).toBeInTheDocument();
-    }, 10000);
+            // Check whether the error message is displayed correctly
+            const displayedErrorMessage = await screen.findByText(errorMsg);
+            expect(displayedErrorMessage).toBeInTheDocument();
+        }, 10000
+    );
 });
