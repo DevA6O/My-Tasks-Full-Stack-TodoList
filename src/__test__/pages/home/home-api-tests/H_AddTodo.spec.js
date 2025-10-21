e7de1ba4a388a5d2877d750d573b84375a9208a5
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-10-21 06:54:14 +0200

    Code readability has been improved

--- src/__test__/pages/home/home-api-tests/H_AddTodo.spec.js
+++ src/__test__/pages/home/home-api-tests/H_AddTodo.spec.js
@@ -2,44 +2,50 @@ import { expect } from "@playwright/test";
 import { fixtures as test } from "../../../apiFixtures.js";
 
 
-test("Add todo was successful", async ({ page, createTodo }) => {
-    // Define title and description
-    const todoTitle = "Test creation todo"
-    const todoDescription = "Test whether the creation was successful"
+test("Todo successfully added", async ({ page }) => {
+    // Define a title and a description
+    const title = "My todo";
+    const description = "My description";
 
-    // Create the tood
-    await createTodo({
-        title: todoTitle,
-        description: todoDescription
-    });
-
-    // Double check
-    const todoTitleElement = page.getByText(todoTitle, {timeout: 5000});
-    await expect(todoTitleElement).toBeVisible();
-
-    const todoDescriptionElement = page.getByText(todoDescription, {timeout: 5000});
-    await expect(todoDescriptionElement).toBeVisible();
-});
-
-test("Add todo failed: Backend validation error", async ({ page }) => {
-    // Define title and description
-    const todoTitle = "X";
-    const todoDescription = "This todo could not be created because the title is too short.";
-
-    // Go to homepage
+    // Go to the homepage
     await page.goto("/");
 
-    // Fill the input fields
-    await page.fill('input[name="title"]', todoTitle);
-    await page.fill('input[name="description"]', todoDescription);
-    
-    // Get submit button and submit the informations
+    // Create the todo
+    const titleInput = page.getByTestId("HomePageAddTodo-Title-Input");
+    const descriptionInput = page.getByTestId("HomePageAddTodo-Description-Input");
     const submitButton = page.getByTestId("HomePageAddTodo-Submit-Button");
-    await expect(submitButton).toBeVisible();
 
+    await titleInput.fill(title);
+    await descriptionInput.fill(description);
     await submitButton.click();
 
-    // Check whether the creation was not successful
-    const errorMessage = page.getByText("Title should have at least 2 characters", {timeout: 5000});
+    // Check whether the creation was successful
+    const createdTodoTitle = page.getByText(title);
+    const createdTodoDescription = page.getByText(description);
+
+    await expect(createdTodoTitle).toBeVisible();
+    await expect(createdTodoDescription).toBeVisible();
+})
+
+
+test("Todo could not be added successfully", async ({ page }) => {
+    // Define a title and a description
+    const title = "X";
+    const description = "The todo could not be added successfully because the title is too short.";
+
+    // Go to the homepage
+    await page.goto("/");
+
+    // Create the todo
+    const titleInput = page.getByTestId("HomePageAddTodo-Title-Input");
+    const descriptionInput = page.getByTestId("HomePageAddTodo-Description-Input");
+    const submitButton = page.getByTestId("HomePageAddTodo-Submit-Button");
+
+    await titleInput.fill(title);
+    await descriptionInput.fill(description);
+    await submitButton.click();
+
+    // Check whether the error message is displayed correctly
+    const errorMessage = page.getByText("Title should have at least 2 characters");
     await expect(errorMessage).toBeVisible();
 });
