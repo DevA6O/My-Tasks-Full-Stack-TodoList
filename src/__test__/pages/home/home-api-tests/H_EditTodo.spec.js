4e30eace85cb64fa7026d560a8eb839e371e3df2
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-10-25 19:26:58 +0200

    Tests are now more precise and easier to read than before

--- src/__test__/pages/home/home-api-tests/H_EditTodo.spec.js
+++ src/__test__/pages/home/home-api-tests/H_EditTodo.spec.js
@@ -4,88 +4,113 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-test("Edit was successful", async ({ page, createTodo }) => {
-    // Go to homepage
+test("Todo successfully edited", async ({ page, createTodo }) => {
+    // // Go to homepage
     await page.goto("/");
 
     // Create a todo
     await createTodo({
-        title: "Edit was successful",
-        description: ""
+        title: "My todo to be edited",
+        description: "This todo will be edited in the test."
     });
 
-    // Get task id
-    const container = page.getByText("Edit was successful")
-        .locator("..") // Go to parent div
-        .locator("..") // Go to task container
-    
+    // Get the container with the created todo
+    const container = page.getByText("My todo to be edited")
+        .locator("..") // Parent div
+        .locator(".."); // Task container
+
     const taskTestID = await container.getAttribute("data-testid");
     const taskID = taskTestID?.split("task-")[1];
 
-    // Click on the edit button
-    await container.locator("button:has-text('Edit')").click();
+    // Open the edit form by clicking the edit button
+    const editButton = page.getByTestId(
+        `HomePageManageAndDisplayTodos-Edit-Button-For-${taskID}`
+    );
+    await editButton.click();
 
-    // Submit new todo description
-    const editDescriptionInput = page.getByTestId(`EditTodo-Description-For-${taskID}`, {timeout: 5000});
-    await expect(editDescriptionInput).toBeVisible();
-    await editDescriptionInput.fill("Edited description");
+    // Edit the todo
+    const editTitleInput = page.getByTestId(`EditTodo-Title-For-${taskID}`);
+    const editDescriptionInput = page.getByTestId(`EditTodo-Description-For-${taskID}`);
+    const editSubmitButton = page.getByTestId(`EditTodo-Submit-Button-For-${taskID}`);
 
-    const editSubmitButton = page.getByTestId(`EditTodo-Submit-Button-For-${taskID}`, {timeout: 5000});
-    await expect(editSubmitButton).toBeVisible();
+    await editTitleInput.fill("Edited todo title");
+    await editDescriptionInput.fill("Edited todo description.");
     await editSubmitButton.click();
 
-    // Check whether the edit was successful
-    const successMessage = page.getByText("Update successful: Todo has been successfully updated.", {timeout: 5000});
+    // Check the success message is displayed
+    const successMessage = page.getByText("Update successful: Todo has been successfully updated.");
     await expect(successMessage).toBeVisible();
-});
+
+    // Check whether the edited todo is displayed correctly
+    const editedTodoTitle = page.getByText("Edited todo title");
+    const editedTodoDescription = page.getByText("Edited todo description.");
+
+    await expect(editedTodoTitle).toBeVisible();
+    await expect(editedTodoDescription).toBeVisible();
+}, { timeout: 10000 });
 
 
-test("Edit failed caused by api response mocking", async ({ page, createTodo, simulateAndMockPostRequestWithRealData }) => {
-    // Go to homepage
+test("Todo could not be edited successfully", async ({ page, createTodo, simulateAndMockPostRequestWithRealData }) => {
+    // // Go to homepage
     await page.goto("/");
 
     // Create a todo
     await createTodo({
-        title: "Edit failed caused by api response mocking",
-        description: ""
+        title: "My todo to be edited",
+        description: "The edit will fail."
     });
-
+   
     // Mock api response
+    const targetURL = `${process.env.VITE_API_URL}/todo/update`;
+
     await simulateAndMockPostRequestWithRealData({
-        url: `${process.env.VITE_API_URL}/todo/update`,
+        url: targetURL,
         headers: {
             "Content-Type": "application/json",
-            "Authorization": "Bearer a-string-secret-at-least-256-bits-long" // Fake access token
         },
         data: {
-            title: "Could not be edited.",
+            title: "Todo could not be edited.",
             description: "",
-            todo_id: "999526e8-8f81-473c-8d62-b84207493ff8" // Fake todo_id
-        }, 
+            todo_id: "00000000-0000-0000-0000-000000000000"
+        },
+        accessToken: true,
         status: 400
     });
 
-    // Get task id
-    const container = page.getByText("Edit was successful")
-        .locator("..") // Go to parent div
-        .locator("..") // Go to task container
+    // Get the container with the created todo
+    const container = page.getByText("My todo to be edited")
+        .locator("..") // Parent div
+        .locator(".."); // Task container
     
     const taskTestID = await container.getAttribute("data-testid");
     const taskID = taskTestID?.split("task-")[1];
 
-    // Click on the edit button
-    await container.locator("button:has-text('Edit')").click();
+    // Open the edit form by clicking the edit button
+    const editButton = page.getByTestId(
+        `HomePageManageAndDisplayTodos-Edit-Button-For-${taskID}`
+    );
+    await editButton.click();
 
-    // Submit new todo description
-    const editDescriptionInput = page.getByTestId(`EditTodo-Title-For-${taskID}`, {timeout: 5000});
-    await expect(editDescriptionInput).toBeVisible();
-    await editDescriptionInput.fill("Could not be edited.");
+    // Try to edit the todo
+    const editTitleInput = page.getByTestId(`EditTodo-Title-For-${taskID}`);
+    const editDescriptionInput = page.getByTestId(`EditTodo-Description-For-${taskID}`);
+    const editSubmitButton = page.getByTestId(`EditTodo-Submit-Button-For-${taskID}`);
 
-    const editSubmitButton = page.getByTestId(`EditTodo-Submit-Button-For-${taskID}`, {timeout: 5000});
-    await expect(editSubmitButton).toBeVisible();
-    await editSubmitButton.click(); 
+    await editTitleInput.fill("My new edited title");
+    await editDescriptionInput.fill("The edit will fail by clicking submit.");
+    await editSubmitButton.click()
 
-    // Check whether the edit was not successful
-    const errorMessage = page.getByText("Update failed: An unexpected error occurred. Please try again later.", {timeout: 5000});
+    // Check whether the edit was unsuccessful
+    const errorMessage = page.getByText("Update failed: Todo could not be found.", { timeout: 2000 });
     await expect(errorMessage).toBeVisible();
-});
+
+    // Reload the page to see whether the todo has been changed
+    await page.reload();
+
+    // Check whether the created todo has not been changed
+    const editedTodoTitle = page.getByText("My todo to be edited");
+    const editedTodoDescription = page.getByText("The edit will fail.");
+
+    await expect(editedTodoTitle).toBeVisible({ timeout: 2000 });
+    await expect(editedTodoDescription).toBeVisible({ timeout: 2000 });
+}, { timeout: 10000 });
