8bbd89aa7d88b2cef1ffc4c3b4970b766c7d62eb
DevA60 <218462454+DevA6O@users.noreply.github.com>
2025-10-25 20:21:42 +0200

    Tests are now more precise and easier to read than before

--- src/__test__/pages/home/home-api-tests/H_ManageAndDisplayTodos.spec.js
+++ src/__test__/pages/home/home-api-tests/H_ManageAndDisplayTodos.spec.js
@@ -5,150 +5,219 @@ import dotenv from "dotenv";
 dotenv.config();
 
 
-test("Completion was successful", async ({ page, createTodo }) => {
+test("Todo marked as completed successfully", async ({ page, createTodo }) => {
+    // Go to homepage
+    await page.goto("/");
+
     // Create todo
     await createTodo({
-        title: "Completion was successful",
-        description: ""
+        title: "My todo to be marked as completed",
+        description: "The completion will be successful."
     });
 
     // Get the container
-    const container = page.getByText("Completion was successful")
-        .locator("..") // Go to parent div (Description & Title)
-        .locator(".."); // Go to task-container
+    const container = page.getByText("My todo to be marked as completed")
+        .locator("..") // Parent div
+        .locator(".."); // Task container
 
-    // Click on complete button
-    await container
-        .locator("button:has-text('Completed')") // find the button with text "Completed"
-        .click(); // Click on the button
+    const taskTestID = await container.getAttribute("data-testid");
+    const taskID = taskTestID?.split("task-")[1];
+
+    // Click on the complete button
+    const completeButton = page.getByTestId(
+        `HomePageManageAndDisplayTodos-Complete-Button-For-${taskID}`
+    );
+    await completeButton.click();
     
     // Check whether the completion was successful
-    const successMessage = page.getByText("Completion successful: Todo has been marked as successfully completed.");
-    await expect(successMessage).toBeVisible({timeout: 5000});
-
-    // Check whether the todo is actually marked as completed
-    const completeButton = container.locator("button:has-text('Completed')")
-    await expect(completeButton).toBeDisabled();
-
-    const editButton = container.locator("button:has-text('Edit')");
-    await expect(editButton).toBeDisabled();
-});
-
-
-test("Completion failed: Authentication - Invalid token", async ({ page, createTodo }) => {
-    await page.route(`${process.env.VITE_API_URL}/todo/complete`, async route => {
-        const response = await route.fetch({
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": "Bearer Invalid-Token"
-            },
-            body: JSON.stringify({
-                todo_id: "00000000-0000-0000-0000-000000000000"
-            })
-        })
-        const realData = await response.json();
-
-        // Return the failed api response
-        await route.fulfill({
-            status: 400,
-            contentType: "application/json",
-            body: JSON.stringify(realData)
-        })
-    })
-
-    // Go to homepage
-    await page.goto("/");
-
-    // Create a todo
-    await createTodo({
-        title: "Completion failed",
-        description: ""
-    });
-
-    await page.getByText("Completion failed")
-        .locator("..") // Go to parent div (Description & Title)
-        .locator("..") // Go to task-container
-        .locator("button:has-text('Completed')") // Find the completed button
-        .click(); // Click on the button
-
-    // Check whether the completion was not successful
-    const errorMessage = page.getByText(
-        "Authentication failed: An unknown error is occurred. Please try again later.", 
-        {timeout: 2000}
+    const successMessage = page.getByText(
+        "Completion successful: Todo has been marked as successfully completed."
     );
-    await expect(errorMessage).toBeVisible();
-});
+    await expect(successMessage).toBeVisible({ timeout: 5000 });
+
+    // // Check whether the todo is actually marked as completed
+
+    // Define the completed button again to avoid stale element handle error
+    const updatedCompleteButton = page.getByTestId(
+        `HomePageManageAndDisplayTodos-Complete-Button-For-${taskID}`
+    );
+    await expect(updatedCompleteButton).toBeDisabled();
+
+    // Check in addition whether the edit button is also disabled
+    const editButton = page.getByTestId(
+        `HomePageManageAndDisplayTodos-Edit-Button-For-${taskID}`
+    );
+    await expect(editButton).toBeDisabled();
+}, { timeout: 10000 });
+
+
+test("Todo could not be marked as completed successfully", async (
+    { page, createTodo, simulateAndMockPostRequestWithRealData }
+) => {
+    // Go to homepage
+    await page.goto("/");
+
+    // Create todo
+    await createTodo({
+        title: "Try to mark todo as completed",
+        description: "The completion will fail."
+    });
+
+    // Mock api response
+    const targetURL = `${process.env.VITE_API_URL}/todo/complete`;
+
+    await simulateAndMockPostRequestWithRealData({
+        url: targetURL,
+        headers: {
+            "Content-Type": "application/json"
+        },
+        data: {
+            title: "My todo to be marked as completed",
+            description: "",
+            todo_id: "00000000-0000-0000-0000-000000000000"
+        },
+        accessToken: true,
+        status: 400
+    });
+
+    // Get the container
+    const container = page.getByText("Try to mark todo as completed")
+        .locator("..") // Parent div
+        .locator(".."); // Task container
+
+    const taskTestID = await container.getAttribute("data-testid");
+    const taskID = taskTestID?.split("task-")[1];
+
+    // Click on the complete button
+    const completeButton = page.getByTestId(
+        `HomePageManageAndDisplayTodos-Complete-Button-For-${taskID}`
+    );
+    await completeButton.click();
+
+    // Check whether the completion was unsuccessful
+    const errorMessage = page.getByText(
+        "Completion failed: Todo could not be found."
+    )
+    await expect(errorMessage).toBeVisible({ timeout: 2000 });
+
+    // Check whether the complete button is still enabled
+
+    // Relaod the page to avoid stale element handle error
+    await page.reload();
+
+    const updatedCompleteButton = page.getByTestId(
+        `HomePageManageAndDisplayTodos-Complete-Button-For-${taskID}`
+    );
+    await expect(updatedCompleteButton).toBeEnabled();
+
+    // Check in addition whether the edit button is also still enabled
+    const editButton = page.getByTestId(
+        `HomePageManageAndDisplayTodos-Edit-Button-For-${taskID}`
+    );
+    await expect(editButton).toBeEnabled();
+}, { timeout: 10000 });
 
 
 
 
-test("Deletion was successful", async ({ page, createTodo }) => {
+test("Todo could be deleted successful", async ({ page, createTodo }) => {
     // Go to homepage
     await page.goto("/");
 
     // Create a todo
     await createTodo({
-        title: "Deletion was successful",
-        description: ""
+        title: "Todo to be deleted",
+        description: "The deletion will be successful."
     });
 
+    // Get the test id
+    const container = await page.getByText("Todo to be deleted")
+        .locator("..") // Parent div
+        .locator(".."); // Task container
+
+    const taskTestID = await container.getAttribute("data-testid");
+    const taskID = taskTestID?.split("task-")[1];
+
     // Click on the delete button
-    await page.getByText("Deletion was successful")
-        .locator("..") // Go to parent div
-        .locator("..") // Go to task container
-        .locator("button:has-text('Delete')") // Find delete button
-        .click(); 
-    
+    const deleteButton = page.getByTestId(
+        `HomePageManageAndDisplayTodos-Delete-Button-For-${taskID}`
+    );
+    await deleteButton.click();
+
     // Check whether the deletion was successful
-    const successMessage = page.getByText("Deletion successful: Todo has been successfully deleted.", {timeout: 5000});
-    await expect(successMessage).toBeVisible();
-});
+    const successMessage = page.getByText(
+        "Deletion successful: Todo has been successfully deleted."
+    );
+    await expect(successMessage).toBeVisible({ timeout: 5000 });
+
+    // Check whether the todo is acutally deleted
+
+    // Reload the page to avoid stale element handle error
+    await page.reload();
+
+    const updatedDeleteButton = page.getByTestId(
+        `HomePageManageAndDisplayTodos-Delete-Button-For-${taskID}`
+    );
+    await expect(updatedDeleteButton).toHaveCount(0);
+}, { timeout: 10000 });
 
 
-test("Deletion failed: Authentication - Invalid token", async ({ page, createTodo }) => {
-    await page.route(`${process.env.VITE_API_URL}/todo/delete`, async route => {
-        // Do a real api request, which fails, because the data aren't correct
-        const response = await route.fetch({
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": "Bearer Invalid-Token"
-            },
-            body: JSON.stringify({
-                todo_id: "00000000-0000-0000-0000-000000000000" // Invalid todo id
-            })
-        });
-        const realData = await response.json();
-        
-        // Return the failed api response
-        await route.fulfill({
-            status: 400,
-            contentType: "application/json",
-            body: JSON.stringify(realData)
-        });
-    });
-
+test("Todo could not be deleted successfully", async (
+    { page, createTodo, simulateAndMockPostRequestWithRealData }
+) => {
     // Go to homepage
     await page.goto("/");
 
     // Create a todo
     await createTodo({
-        title: "Deletion failed",
-        description: ""
+        title: "Todo could not be deleted",
+        description: "The deletion will be fail."
     });
 
+    // Mock api response
+    const targetURL = `${process.env.VITE_API_URL}/todo/delete`;
+
+    await simulateAndMockPostRequestWithRealData({
+        url: targetURL,
+        headers: {
+            "Content-Type": "application/json"
+        },
+        data: {
+            title: "Todo could not be deleted",
+            description: "",
+            todo_id: "00000000-0000-0000-0000-000000000000"
+        },
+        accessToken: true,
+        status: 400
+    });
+
+    // Get the test id
+    const container = await page.getByText("Todo could not be deleted")
+        .locator("..") // Parent div
+        .locator(".."); // Task container
+
+    const taskTestID = await container.getAttribute("data-testid");
+    const taskID = taskTestID?.split("task-")[1];
+
     // Click on the delete button
-    await page.getByText("Deletion failed")
-        .locator("..") // Go to parent div
-        .locator("..") // Go to task container
-        .locator("button:has-text('Delete')") // Find delete button
-        .click(); 
-    
+    const deleteButton = page.getByTestId(
+        `HomePageManageAndDisplayTodos-Delete-Button-For-${taskID}`
+    );
+    await deleteButton.click();
+
     // Check whether the deletion was unsuccessful
     const errorMessage = page.getByText(
-        "Authentication failed: An unknown error is occurred. Please try again later.", 
-        {timeout: 2000}
+        "Deletion failed: Todo could not be found."
     );
-    await expect(errorMessage).toBeVisible();
-})
+    await expect(errorMessage).toBeVisible({ timeout: 2000 });
+
+    // Check whether the todo is acutally not deleted
+
+    // Reload the page to avoid stale element handle error
+    await page.reload();
+
+    const updatedDeleteButton = page.getByTestId(
+        `HomePageManageAndDisplayTodos-Delete-Button-For-${taskID}`
+    );
+    await expect(updatedDeleteButton).toHaveCount(1);
+}, { timeout: 10000 });
